c54192b08e29a11795f568895c10a2a29942883d
Anggi Widiantara <widiantara2906@gmail.com>
2024-09-11 21:10:38 +0800

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: [
+        "./src/**/*.html", // This will capture HTML files inside 'src'
+        "./index.html", // Specifically include the 'index.html' file in the root
+        "./src/**/*.js", // Capture JS files inside 'src'
+    ],
     theme: {
         extend: {
             fontFamily: {
@@ -13,6 +17,8 @@ module.exports = {
                 text: "#000000",
                 gray: "#F0F0F0",
                 text_gray: "#888888",
+                green: "#00BE1E",
+                red: "#D60000",
             },
         },
     },
